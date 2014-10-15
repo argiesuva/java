440c1b0af76438c09660019e2bd8d78f2d19afc6
tanakaedu <tanaka@tama.ac.jp>
2014-10-15 16:06:37 +0900

    keisanを追加

---
+++ keisan.java
@@ -0,0 +1,22 @@
+class keisan {
+	public static void main(String args[])
+	{
+    	// “ü—ÍF‚Q‚Â‚Ì”’l‚ğ“ü—Í
+    	int in0 = new java.util.Scanner(System.in).nextInt();
+    	int in1 = new java.util.Scanner(System.in).nextInt();
+    	System.out.println(in0+","+in1);
+    	
+    	// ˆ—F‘«‚µZ
+    	int add = in0+in1;
+    	// ˆ—Fˆø‚«Z
+    	int sub = in0-in1;
+    	// ˆ—FŠ|‚¯Z
+    	// ˆ—FŠ„‚èZ
+    	// ˆ—F—]‚è
+    	
+    	// ŒvZŒ‹‰Ê‚ğo—Í
+    	System.out.println("‘«‚µZF"+add);
+    	System.out.println("ˆø‚«ZF"+sub);
+	}
+}
+
