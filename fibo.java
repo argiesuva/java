a0024e87d4cff655b0c3e837786de741c36535ba
tanakaedu <tanaka@am1.jp>
2014-11-12 16:19:34 +0900

    fibo完成。次回、1～100まで合計

--- fibo.java
+++ fibo.java
@@ -1,6 +1,23 @@
-class pracfor {
+class fibo {
 	public static void main(String args[])
 	{
+		// フィボナッチ数列(0～10項まで算出して表示)
+		//// 配列の定義
+		int [] hai = new int[11];
+		//// 0項目の初期化
+		hai[0] = 0;
+		//// 1項目の初期化
+		hai[1] = 1;
+		//// 2～10項の計算
+		///// 2～10までのループをfor文で書く
+		for (int i=2 ; i<=10 ; i++){
+			hai[i] = hai[i-1] + hai[i-2];
+		}
+		//// 1～10項までを表示
+		for (int i=1 ; i<=10 ; i++){
+		     System.out.println(hai[i]);
+		}
+		/* 5～8までのループ
 		for (int i=5 ; i<=8 ; i++) {
 			System.out.println(i);
 		}
@@ -17,6 +34,7 @@ class pracfor {
 		for (int i=0 ; i<=9 ; i++) {
 			System.out.println(hai[i]);
 		}
+		*/
 	}
 }
 
