2ddf02ae93d048275be39f89a1793b546dd5608a
tanakaedu <tanaka@tama.ac.jp>
2014-11-26 16:20:06 +0900

    BigIntegerの利用を途中まで

--- lucas.java
+++ lucas.java
@@ -1,3 +1,5 @@
+import java.math.*;
+
 // 検索ワード：
 // 使うクラス：
 
@@ -6,13 +8,14 @@ class lucas {
 	{
 		// リュカ数列(1～100項まで算出して、合計を表示)
 		// 合計を入れる変数を定義して、0を代入しておく
-		double ryuka = 0;
-		//// 配列の定義(100項まで)
-		double [] hai = new double[101];
+		// BigInteger型でryukaを定義して0を代入
+		BigInteger ryuka = new BigInteger("0");
+		//// BigInteger型の配列の定義(100項まで)
+		BigInteger [] hai = new BigInteger[101];
 		//// 0項目の初期化
-		hai[0] = 2;
+		hai[0] = new BigInteger("2");
 		//// 1項目の初期化
-		hai[1] = 1;
+		hai[1] = new BigInteger("1");
 		//// 2～100項の計算
 		///// 2～100までのループをfor文で書く
 		for (int i=2 ; i<=100 ; i++){
@@ -22,25 +25,6 @@ class lucas {
 			// 足した値を表示
 			System.out.println(ryuka);
 		}
-
-		/* 5～8までのループ
-		for (int i=5 ; i<=8 ; i++) {
-			System.out.println(i);
-		}
-		
-		int [] hai = new int[10];
-		hai[0] = 0;
-		hai[1] = 10;
-		hai[2] = 100
-		System.out.println(hai[0]+","+hai[1]+","+hai[2]);
-		
-		for (int i=0 ; i<=9 ; i++) {
-			hai[i] = i*2;
-		}
-		for (int i=0 ; i<=9 ; i++) {
-			System.out.println(hai[i]);
-		}
-		*/
 	}
 }
 
