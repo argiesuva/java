51c060ce44226cbbe77b7cff312501c2d07af5c5
tanakaedu <tanaka@tama.ac.jp>
2014-12-10 16:07:18 +0900

    100個の乱数をテキストファイルに出力する処理をfile.javaに実装

--- file.java
+++ file.java
@@ -10,15 +10,23 @@ class file {
 		// 0～100までの乱数をint型の変数aに求めて
 		// 出力
 		Random b = new Random();
-		int a =b.nextInt(101);
-		System.out.print(a);
+		
 		// 変数aをtensu.txtにファイル出力する
 		try{
-			File file = new File("tensu.txt");
+			// tensu.txtに書き込む準備
+			File file = new File("tensu.csv");
 			FileWriter fw = new FileWriter(file);
 			BufferedWriter bw = new BufferedWriter(fw);
 			PrintWriter pw = new PrintWriter(bw);
-			pw.println(a);
+			// 100個乱数をファイルに書き出す
+			for(int i=1; i<=100;i++)
+			{
+				// 乱数を生成
+				int a =b.nextInt(101);
+				// ファイルに値を書き込む
+				pw.print(a+",");
+			}
+			// ファイルを閉じる
 			pw.close();
 		} catch(Exception e)
 		{
